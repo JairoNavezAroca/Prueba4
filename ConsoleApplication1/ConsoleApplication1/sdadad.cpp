ff036074997d4113dd3763ccab7d9b6c2d36240a
Jairo Navez <jairowolf123@hotmail.com>
2019-04-28 22:10:21 -0500

    1
    2

---
+++ ConsoleApplication1/ConsoleApplication1/sdadad.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "sdadad.h"
+
+
+sdadad::sdadad()
+{
+}
+
+
+sdadad::~sdadad()
+{
+}
---
+++ ConsoleApplication1/ConsoleApplication1/sdadad.h
@@ -0,0 +1,8 @@
+#pragma once
+class sdadad
+{
+public:
+	sdadad();
+	~sdadad();
+};
+
